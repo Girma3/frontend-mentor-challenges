7b4b1260e801dca8dde29e8a43ca00ca32e6dcae
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-11-08 17:04:42 +0300

    function created to close filter on card clicked

--- rest-countries-api/src/components/Card.jsx
+++ rest-countries-api/src/components/Card.jsx
@@ -1,22 +1,46 @@
 import styles from "./Card.module.css";
+import { Link } from "react-router-dom";
+import { useTheme } from "./ThemeContext";
+function Card({ data = {}, onAction }) {
+  const { theme } = useTheme();
 
-function Card({ data }) {
+  let countryName = "";
+  function checkData() {
+    if (data.name.common === undefined) {
+      countryName = data.name;
+    } else {
+      countryName = data.name.common;
+    }
+  }
+  checkData();
+
   return (
-    <button className={styles["card-holder"]}>
-      <div className={styles.flag}>image</div>
-      <div className={`flex-column ${styles["detail-holder"]}`}>
-        <h1 className={styles.name}>{data.name}</h1>
-        <p className={styles.detail}>
-          population: <span className={styles.info}>{data.population}</span>{" "}
-        </p>
-        <p className={styles.detail}>
-          Region: <span className={styles.info}>{data.region}</span>{" "}
-        </p>
-        <p className={styles.detail}>
-          capital: <span className={styles.info}>{data.capital}</span>
-        </p>
-      </div>
-    </button>
+    <li
+      className={` ${theme === "light" ? styles.lightMode : styles.darkMode} 
+     ${styles["card-holder"]}
+      `}
+      onClick={onAction}
+    >
+      <Link to={`/countries/${countryName}`}>
+        <img
+          src={`${data.flags.png}`}
+          alt={`${data.flags.alt}`}
+          className={styles.flag}
+        />
+        <div className={`flex-column ${styles["detail-holder"]}`}>
+          <h1 className={styles.name}>{data.name.common || data.name}</h1>
+          <p className={styles.detail}>
+            population: <span className={styles.info}>{data.population}</span>{" "}
+          </p>
+          <p className={styles.detail}>
+            Region: <span className={styles.info}>{data.region}</span>{" "}
+          </p>
+          <p className={styles.detail}>
+            capital: <span className={styles.info}>{data.capital}</span>
+          </p>
+        </div>{" "}
+      </Link>
+    </li>
   );
 }
 
--- rest-countries-api/src/components/Filter.jsx
+++ rest-countries-api/src/components/Filter.jsx
@@ -2,18 +2,29 @@ import { Link } from "react-router-dom";
 import styles from "./Filter.module.css";
 import { useState } from "react";
 import Button from "./Button";
+import { useTheme } from "./ThemeContext";
 
-function Filter({ onFilter }) {
-  const [show, setShow] = useState(false);
+function Filter({ onFilter, show, setShow }) {
+  // const [show, setShow] = useState(false);
+  const { theme } = useTheme();
   return (
     <div className={styles.wrapper}>
       <div>
-        <Button onAction={() => setShow(() => !show)}>
+        <Button
+          className={`${styles.filter}  ${
+            theme === "light" ? styles.lightMode : styles.darkMode
+          }`}
+          onAction={() => setShow(() => !show)}
+        >
           Filter by Region <span>^</span>
         </Button>
 
         {show && (
-          <ul className={styles["dropdown-content"]}>
+          <ul
+            className={`${styles["dropdown-content"]}  ${
+              theme === "light" ? styles.lightMode : styles.darkMode
+            }`}
+          >
             <li className={styles["filter-li"]}>
               <Button className={styles["filter-btn"]} onAction={onFilter}>
                 Africa
