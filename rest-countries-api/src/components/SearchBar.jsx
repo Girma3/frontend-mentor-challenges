a3cefe1e4e9e8789744457c56c576d51df7308b5
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-11-09 16:15:48 +0300

    max length for input added

--- rest-countries-api/src/components/SearchBar.jsx
+++ rest-countries-api/src/components/SearchBar.jsx
@@ -1,6 +1,4 @@
 import styles from "./SearchBar.module.css";
-
-import { FaMagnifyingGlass } from "react-icons/fa6";
 import { useTheme } from "./ThemeContext";
 import { useEffect, useRef } from "react";
 function SearchBar({ onAction, query, msg }) {
