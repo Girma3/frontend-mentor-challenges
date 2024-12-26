614b77b81c96b5812f216b4726474ded977c98ff
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-12-26 16:25:29 +0300

    styles: media query style changed for mobile

--- shopping-cart/src/components/Footer.jsx
+++ shopping-cart/src/components/Footer.jsx
@@ -4,12 +4,24 @@ function Footer() {
   return (
     <footer>
       <PageNav navClass={styles.nav} linkClass={styles.links} />
-      <p className={styles.aboutCompany}>
-        Audiophile is an all in one stop to fulfill your audio needs.We&apos;re
-        a small team of music lovers and sound specialists who are devoted to
-        helping you get the most out of personal audio.Come and visit our demo
-        facility - We&apos;re open 7 days a week.{" "}
-      </p>
+      <div></div>
+      <div className={`${styles.flexRow} ${styles.companyInfo}`}>
+        <div className={styles.companyParHolder}>
+          <p className={styles.aboutCompany}>
+            Audiophile is an all in one stop to fulfill your audio
+            needs.We&apos;re a small team of music lovers and sound specialists
+            who are devoted to helping you get the most out of personal
+            audio.Come and visit our demo facility - We&apos;re open 7 days a
+            week.{" "}
+          </p>
+          <p className={styles.aboutCompany}>
+            copyright 2024 ALL RIGHT RESERVED.
+          </p>
+        </div>
+        <div className={styles.socialMedia}>
+          <p className={`${styles.flexRow} ${styles.socialIcons}`}>telegram</p>{" "}
+        </div>
+      </div>
     </footer>
   );
 }
