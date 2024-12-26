1ce53f0a243d86f7d89283da582fcc07c277e32d
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-12-26 16:32:18 +0300

    feat: link to hompage added when cart is empty

--- shopping-cart/src/components/CartStatus.jsx
+++ shopping-cart/src/components/CartStatus.jsx
@@ -1,53 +1,81 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./CartStatus.module.css";
+import propTypes from "prop-types";
+import { Link } from "react-router-dom";
+import CartItem from "./CartItem";
+import { getProductData, calculateTotalPrice } from "../utilityFunctions";
 
-function CartStatus({ show, cartProducts, allProducts }) {
+function CartStatus({
+  productsData,
+  allCartProducts,
+  onIncQuantity,
+  onDecQuantity,
+  onRemoveAll,
+}) {
   const navigate = useNavigate();
   function handleCheckout() {
     navigate("/checkOut");
   }
 
-  let allCartProducts = cartProducts;
+  let cartProductData = getProductData(productsData, allCartProducts);
+  let totalPrice = calculateTotalPrice(allCartProducts);
 
-  function getProductData(allProducts, cartProducts) {
-    return allProducts.filter((product) =>
-      cartProducts.some((cartItem) => cartItem.productName === product.slug)
-    );
-  }
-
-  let cartProductData = getProductData(allProducts, allCartProducts);
-  console.log(allCartProducts);
-  console.log(allProducts);
-  console.log(cartProductData);
   return (
-    <div className={`${styles.flexColumn} ${styles.statusHolder}`}>
+    <>
       <div className={`${styles.cartInfo} ${styles.flexRow}`}>
-        <h2 className={styles.cartItems}>CART({cartProducts.length})</h2>
-        <button className={styles.removeAllBtn}>Remove All</button>
+        <h2 className={styles.cartItems}>CART({allCartProducts.length})</h2>
+        <button
+          className={styles.removeAllBtn}
+          onClick={onRemoveAll}
+          disabled={allCartProducts.length ? false : true}
+        >
+          Remove All
+        </button>
       </div>
-      <div className={`${styles.productHolder} ${styles.flexRow}`}>
-        <div className={styles.imageHolder}></div>
-        <div className={`${styles.productDetail} ${styles.flexColumn}`}>
-          <p className={styles.productName}>xx</p>
-          <p className={styles.price}>$123</p>
-        </div>
-        <div className={`${styles.btnHolder} ${styles.flexRow}`}>
-          <button className={styles.decBtn}>-</button>
-          <h3 className={styles.quantity}>12</h3>
-          <button className={styles.incBtn}>+</button>
-        </div>
-      </div>
+      {!allCartProducts.length && (
+        <>
+          <h2>Your Cart is Empty.</h2>
+          <p>
+            continue shopping on the <b>audiophile</b> website{" "}
+            <Link to="/">homepage</Link>{" "}
+          </p>
+        </>
+      )}
+      <ul>
+        {cartProductData.map((product, i) => (
+          <CartItem
+            key={i}
+            product={product}
+            quantity={allCartProducts[i].demand}
+            onDecQuantity={() => onDecQuantity(allCartProducts[i].productName)}
+            onIncQuantity={() => onIncQuantity(allCartProducts[i].productName)}
+            showButton={true}
+          />
+        ))}
+      </ul>
+
       <div className={`${styles.flexColumn} ${styles.totalHolder}`}>
         <div className={`${styles.flexRow} ${styles.totalHolder}`}>
           <h2 className={styles.total}>TOTAL</h2>
-          <p className={styles.totalPrice}>$ 2345</p>
+          <p className={styles.totalPrice}>$ {totalPrice}</p>
         </div>
-        <button className={styles.checkOutBtn} onClick={handleCheckout}>
+
+        <button
+          className={styles.checkOutBtn}
+          onClick={handleCheckout}
+          disabled={allCartProducts.length ? false : true}
+        >
           CHECKOUT
         </button>
       </div>
-    </div>
+    </>
   );
 }
-
+CartStatus.propTypes = {
+  productsData: propTypes.array,
+  allCartProducts: propTypes.array,
+  onIncQuantity: propTypes.func,
+  onDecQuantity: propTypes.func,
+  onRemoveAll: propTypes.func,
+};
 export default CartStatus;
