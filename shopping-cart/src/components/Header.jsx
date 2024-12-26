523231bdce3f1e40d85b6bd472c54733c39f1e41
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-12-26 16:39:13 +0300

    feat: for mobile device prop added to show menu

--- shopping-cart/src/components/Header.jsx
+++ shopping-cart/src/components/Header.jsx
@@ -1,42 +1,27 @@
-import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 import propTypes from "prop-types";
-import ProductNav from "./ProductNav";
-
-function Header({ deviceSize = window.innerWidth }) {
-  const [show, setShow] = useState(false);
-
-  function handleMobileMenu() {
-    setShow(() => !show);
-  }
-  const [screenSize, setScreenSize] = useState(deviceSize);
-  useEffect(() => {
-    const handleResize = () => {
-      setScreenSize(window.innerWidth);
-    };
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
+function Header({
+  show = false,
+  deviceSize = window.innerWidth,
+  onMobileMenu,
+}) {
   return (
     <nav className={styles.header} aria-live="polite">
-      {screenSize < 760 && (
+      {deviceSize < 760 && (
         <section className={styles.navHolder}>
           {show ? (
             <>
               <button
                 data-show={show}
-                onClick={handleMobileMenu}
+                onClick={onMobileMenu}
                 aria-label="open-menu"
               ></button>
             </>
           ) : (
             <button
               data-show={show}
-              onClick={handleMobileMenu}
+              onClick={onMobileMenu}
               aria-label="close-menu"
             ></button>
           )}
@@ -52,10 +37,10 @@ function Header({ deviceSize = window.innerWidth }) {
             <span className={styles.letter}>l</span>
             <span className={styles.letter}>e</span>
           </div>
-          <button className={styles.cart}></button>{" "}
         </section>
       )}
-      {screenSize > 760 && (
+
+      {deviceSize > 760 && (
         <section className={styles.nav}>
           <Link to="/" className={styles.logo}>
             <span className={styles.letter}>a</span>
@@ -87,18 +72,14 @@ function Header({ deviceSize = window.innerWidth }) {
           </nav>
         </section>
       )}
-
-      {show && screenSize < 760 && (
-        <ul className={`${styles.cardLinks} ${styles.flexRow}`}>
-          <ProductNav />
-        </ul>
-      )}
     </nav>
   );
 }
 
 Header.propTypes = {
   deviceSize: propTypes.number,
+  show: propTypes.bool,
+  onMobileMenu: propTypes.func,
 };
 
 export default Header;
