33199e50c25e1a4a66f830d41c42351ed95553d5
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-12-26 16:34:58 +0300

    feat: suggestion title added for product list

--- shopping-cart/src/components/ProductDetail.jsx
+++ shopping-cart/src/components/ProductDetail.jsx
@@ -1,48 +1,118 @@
-import AboutCompany from "./AboutCompany";
-import CardLink from "./CardLink";
 import styles from "./ProductDetail.module.css";
+import AboutCompany from "./AboutCompany";
+import ProductNav from "./ProductNav";
 import ProductSuggestCard from "./ProductSuggestCard";
 import { useNavigate, useParams } from "react-router-dom";
-function ProductDetail() {
-  const { name } = useParams();
+import propTypes from "prop-types";
+
+function findCategory(array, name) {
+  const item = array.filter((product) => product.slug === name);
+  return item[0].category;
+}
+// component render clicked product using given product array and using it's name passed
+//  list suggestion of other product using all products data array
+function ProductDetail({
+  productsData,
+  allProductsData,
+  stockProducts,
+  onIncProductQuantity,
+  onDecProductQuantity,
+  onAddToCart,
+  onQuantityInput,
+}) {
+  let { name } = useParams();
   const navigate = useNavigate();
   function handleBackBtn() {
     navigate(-1);
   }
-  console.log(name);
+
+  let product = productsData.filter((product) => product.slug === name)[0];
+  let selectedProduct = stockProducts.filter(
+    (product) => product.productName === name
+  )[0];
+
+  function disableBtn() {
+    return selectedProduct.inStock > selectedProduct.demand;
+  }
+
+  const features = product.features;
+  let index = features.indexOf("\n\n");
+  let part1 = features.substring(0, index);
+  let part2 = features.substring(index + 2);
+
+  if (product === undefined) return <p>no product</p>;
+
   return (
-    <>
+    <main>
       <section>
         <button className={styles.backBtn} onClick={handleBackBtn}>
           Go Back
         </button>
         <section className={styles.flexRow}>
           <div className={styles.image}>
-            <img
-              src="/assets/product-xx99-mark-two-headphones/desktop/image-product.jpg"
-              alt="ear"
-              className={styles.productImage}
-            />
+            <picture>
+              <source
+                media="(min-width: 760)"
+                srcSet={`${product.image.desktop}`}
+              />
+              <source
+                media="(min-width:500 )"
+                srcSet={`${product.image.tablet}`}
+              />
+
+              <img
+                src={`${product.image.mobile}`}
+                alt="ear"
+                className={styles.productImage}
+              />
+            </picture>
           </div>
-          <div className={`${styles.productAddHolder} ${styles.flexColum}`}>
-            <h2 className={styles.productNew}>NEW PRODUCT</h2>
-            <h2 className={styles.productNAme}>xx99 mark II headphones</h2>
-            <p className={styles.productDescription}>
-              {" "}
-              The new XX99 Mark II headphones is the pinnacle of pristine audio.
-              It redefines your premium headphone experience by reproducing the
-              balanced depth and precision of studio-quality sound.,
-            </p>
+          <div className={`${styles.productAddHolder} ${styles.flexColumn}`}>
+            {product.isNew && (
+              <h2 className={styles.productNew}>NEW PRODUCT</h2>
+            )}
+            <h2 className={styles.productNAme}>{name}</h2>
+            <p className={styles.productDescription}>{product.description}</p>
             <p className={styles.price}>$ 5678</p>
 
             <div className={`${styles.flexRow} ${styles.btnHolder}`}>
-              <div className={styles.flexRow}>
-                <button className={styles.decBtn}>-</button>
-                <h2>0</h2>
-                <button className={styles.incBtn}>+</button>
+              <div className={`${styles.flexRow} ${styles.quantityHolder}`}>
+                <button
+                  className={styles.decBtn}
+                  onClick={() => onDecProductQuantity(name)}
+                >
+                  -
+                </button>
+                <label
+                  htmlFor="productDemand"
+                  className={styles.hideLabel}
+                ></label>
+                <input
+                  type="number"
+                  name="productDemand"
+                  id="productDemand"
+                  value={Number(selectedProduct.demand)}
+                  aria-label="product demand quantity"
+                  className={styles.productQuantity}
+                  onChange={(e) => onQuantityInput(e, name)}
+                />
+                <button
+                  className={styles.incBtn}
+                  onClick={() => onIncProductQuantity(name)}
+                >
+                  +
+                </button>
               </div>
               <div className={styles.btnHolder}>
-                <button className={styles.addToCartBtn}>
+                <button
+                  className={`${styles.addToCartBtn} ${
+                    selectedProduct.inStock > selectedProduct.demand
+                      ? ""
+                      : styles.disableBtn
+                  }`}
+                  disabled={!disableBtn()}
+                  onClick={() => onAddToCart(name)}
+                >
                   <span>ADD TO CART</span>{" "}
                 </button>
               </div>
@@ -50,79 +120,109 @@ function ProductDetail() {
           </div>
         </section>
         <section className={`${styles.flexRow} ${styles.featHolder}`}>
-          <div className={styles.flexColum}>
+          <article className={styles.flexColumn}>
             <h2>FEATURES</h2>
-            <p className={styles.productFeat}>
-              Featuring a genuine leather head strap and premium earcups, these
-              headphones deliver superior comfort for those who like to enjoy
-              endless listening. It includes intuitive controls designed for any
-              situation. Whether you’re taking a business call or just in your
-              own personal space, the auto on/off and pause features ensure that
-              you’ll never miss a beat.
-            </p>
-            <p className={styles.productFeat}>
-              The advanced Active Noise Cancellation with built-in equalizer
-              allow you to experience your audio world on your terms. It lets
-              you enjoy your audio in peace, but quickly interact with your
-              surroundings when you need to. Combined with Bluetooth 5. 0
-              compliant connectivity and 17 hour battery life, the XX99 Mark II
-              headphones gives you superior sound, cutting-edge technology, and
-              a modern design aesthetic.
-            </p>
-          </div>
-          <div className={`${styles.flexColum} ${styles.inBox}`}>
+            <p className={`${styles.productFeat}`}>{part1}</p>
+            <p className={`${styles.productFeat}`}>{part2}</p>
+          </article>
+          <div className={`${styles.flexColumn} ${styles.inBox}`}>
             <h2>IN THE BOX</h2>
-            <p>
-              <span>x1</span> <span>unit</span>
-            </p>
-            <p>
-              <span>x1</span> <span>unit</span>
-            </p>
-            <p>
-              <span>x1</span> <span>unit</span>
-            </p>
-            <p>
-              <span>x1</span> <span>unit</span>
-            </p>
-            <p>
-              <span>x1</span> <span>unit</span>
-            </p>
+            <ul>
+              {product.includes.map((items, i) => (
+                <li key={i}>
+                  <p>
+                    <span> {items.quantity}x</span>
+                    <span>{items.item}</span>
+                  </p>
+                </li>
+              ))}
+            </ul>
           </div>
         </section>
         <section className={styles.productGallery}>
           <div className={styles.productGalleryImageOne}>
-            <img
-              src="/assets/product-xx99-mark-two-headphones/desktop/image-gallery-1.jpg"
-              alt="er"
-              className={styles.galleyImage}
-            />
+            <picture>
+              <source
+                media="(min-width: 760)"
+                srcSet={`${product.gallery[0].desktop}`}
+              />
+              <source
+                media="(min-width: 500)"
+                srcSet={`${product.gallery[0].tablet}`}
+              />
+
+              <img
+                src={`${product.gallery[0].mobile}`}
+                alt={name}
+                className={styles.galleyImage}
+              />
+            </picture>
           </div>
           <div className={styles.productGalleryImageTwo}>
-            <img
-              src="/assets/product-xx99-mark-two-headphones/desktop/image-gallery-2.jpg"
-              alt="yu"
-              className={styles.galleyImage}
-            />
+            <picture>
+              <source
+                media="(min-width: 760)"
+                srcSet={`${product.gallery[1].desktop}`}
+              />
+              <source
+                media="(min-width: 500)"
+                srcSet={`${product.gallery[1].tablet}`}
+              />
+
+              <img
+                src={`${product.gallery[1].mobile}`}
+                alt={name}
+                className={styles.galleyImage}
+              />
+            </picture>
           </div>
           <div className={styles.productGalleryImageThree}>
-            <img
-              src="/assets/product-xx99-mark-two-headphones/desktop/image-gallery-3.jpg"
-              alt="a"
-              className={`${styles.galleyImage} ${styles.gallerySecondImage}`}
-            />
+            <picture>
+              <source
+                media="(min-width: 760)"
+                srcSet={`${product.gallery[2].desktop}`}
+              />
+              <source
+                media="(min-width: 500)"
+                srcSet={`${product.gallery[2].tablet}`}
+              />
+
+              <img
+                src={`${product.gallery[2].mobile}`}
+                alt={name}
+                className={`${styles.galleyImage} ${styles.gallerySecondImage}`}
+              />
+            </picture>
           </div>
         </section>
         <section>
+          <h2 className={styles.suggestTitle}>YOU MAY ALSO LIKE</h2>
+          <ul className={`${styles.flexRow} ${styles.suggestProducts}`}>
+            {product.others.map((item, i) => (
+              <ProductSuggestCard
+                key={i}
+                suggestedProduct={item}
+                category={findCategory(allProductsData, item.slug)}
+              />
+            ))}
+          </ul>{" "}
           <ul>
-            <CardLink />
-          </ul>
-          <ul>
-            <ProductSuggestCard />
+            <ProductNav />
           </ul>
         </section>
         <AboutCompany />
       </section>
-    </>
+    </main>
   );
 }
+
+ProductDetail.propTypes = {
+  productsData: propTypes.array,
+  allProductsData: propTypes.array,
+  stockProducts: propTypes.array,
+  onIncProductQuantity: propTypes.func,
+  onDecProductQuantity: propTypes.func,
+  onAddToCart: propTypes.func,
+  onQuantityInput: propTypes.func,
+};
 export default ProductDetail;
