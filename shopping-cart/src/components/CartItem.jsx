301c492f08f43a51848c2aba6b9a2592aa786170
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-12-26 16:33:32 +0300

    feat: button render using props

--- shopping-cart/src/components/CartItem.jsx
+++ shopping-cart/src/components/CartItem.jsx
@@ -1,24 +1,34 @@
 import styles from "./CartItem.module.css";
-import { adjustName } from "../functions";
-function CartItem({ product, quantity = 2, onIncQuantity, onDecQuantity }) {
+import { adjustName } from "../utilityFunctions";
+function CartItem({
+  product,
+  quantity = 2,
+  showButton = true,
+  onIncQuantity,
+  onDecQuantity,
+}) {
   return (
     <li className={`${styles.productHolder} ${styles.flexRow}`}>
       <div className={styles.imageHolder}>
         <img src={product.image.mobile} alt={product.name} />
       </div>
+
       <div className={`${styles.productDetail} ${styles.flexColumn}`}>
         <p className={styles.productName}>{adjustName(product.name)}</p>
         <p className={styles.price}>${product.price}</p>
       </div>
-      <div className={`${styles.btnHolder} ${styles.flexRow}`}>
-        <button className={styles.decBtn} onClick={onDecQuantity}>
-          -
-        </button>
-        <h3 className={styles.quantity}>{quantity}</h3>
-        <button className={styles.incBtn} onClick={onIncQuantity}>
-          +
-        </button>
-      </div>
+      {showButton && (
+        <div className={`${styles.btnHolder} ${styles.flexRow}`}>
+          <button className={styles.decBtn} onClick={onDecQuantity}>
+            -
+          </button>
+          <h3 className={styles.quantity}>{quantity}</h3>
+          <button className={styles.incBtn} onClick={onIncQuantity}>
+            +
+          </button>
+        </div>
+      )}
+      {!showButton && <h3 className={styles.quantity}>x{quantity}</h3>}
     </li>
   );
 }
