7045c5c7de785f21736ee9b2e2ecb7bf802dd205
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-12-27 18:12:10 +0300

    feat:pops added to close mobile nav on click

--- shopping-cart/src/components/CardLink.jsx
+++ shopping-cart/src/components/CardLink.jsx
@@ -2,34 +2,48 @@ import { Link, useNavigate } from "react-router-dom";
 import styles from "./CardLink.module.css";
 import propTypes from "prop-types";
 function CardLink({
-  linkName = "HOME",
+  linkName = "earphones",
   productImage = "/assets/shared/image-category-thumbnail-earphones.png",
-  productImageAlt = "EARPHONE",
+  product = "yx1-earphones",
+  onCloseSelf
 }) {
   const navigate = useNavigate();
+
   function handleRedirectPage() {
     navigate(`/${linkName}`);
+    onCloseSelf()
   }
   return (
-    <Link to={`/${linkName.toLowerCase()}`}>
+    <button className={styles.linkButton} onClick={handleRedirectPage}>
       <li className={styles.card}>
         <div className={styles.productLinkImage}>
-          <img src={`${productImage}`} alt={productImageAlt} />
+          <img src={`${productImage}`} alt={product} />
         </div>
         <div className={styles.linkHolder}>
           <h2 className={styles.productName}>{linkName}</h2>
-          <button className={styles.shopBtn} onClick={handleRedirectPage}>
-            shop &gt;{" "}
-          </button>
+          <Link to={`/${linkName}/${product}`} className={styles.link}>
+            <span>SHOP</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className={styles.shopSvg}
+            >
+              <path d="M9 18l6-6-6-6" />
+            </svg>
+          </Link>
         </div>
       </li>
-    </Link>
+    </button>
   );
 }
 CardLink.propTypes = {
   linkName: propTypes.string.isRequired,
   productImage: propTypes.string,
   productImageAlt: propTypes.string,
+  product: propTypes.string,
+  onCloseSelf:propTypes.func
 };
 
 export default CardLink;
