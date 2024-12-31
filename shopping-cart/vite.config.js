f3b7587f09ef4d901ec341da09ca608655ff80ef
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-12-31 10:09:33 +0300

    redirect page on deployment

--- shopping-cart/vite.config.js
+++ shopping-cart/vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./__tests__/setup.js",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {},
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    rollupOptions: {
+      output: {},
+    },
   },
 });
